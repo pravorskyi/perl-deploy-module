35e75556d3573315476e4a264625951e67d7862f
Pravorskyi Andrii <38796092+pravorskyi@users.noreply.github.com>
2018-04-28 18:33:05 +0300

    Add options "action" and "application"

--- perl-deploy-module.pl
+++ perl-deploy-module.pl
@@ -1,22 +1,37 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Getopt::Long;
+use Switch;
 
 sub deploy($)
 {
 	my $path = shift;
-	print "Deploying \"".$path."\"...";
+	print "Deploying \"".$path."\"...\n";
 }
 
 sub undeploy($)
 {
 	my $path = shift;
-	print "Undeployin \"".$path."\"...";
+	print "Undeploying \"".$path."\"...\n";
 }
 
 sub main
 {
-	deploy("/usr/share/tomcat8-docs/docs/appdev/sample/sample.war");
+	my $action = "";
+	my $application = "";
+
+	GetOptions( "action=s" => \$action,
+				 "application=s" => \$application )
+		or die "Error in command line arguments.\n";
+
+	switch( $action )
+	{
+		case "deploy" { deploy( $application ) }
+		case "undeploy" { undeploy ($application ) } 
+		else { die "Invalid option \"action\"." }
+	}
+
 	return 0;
 }
 
